3fdf7a5f6d86dcdf3a7908d14374bc1fbf58ed2b
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-05-23 09:22:09 +0900

    ordersコントローラーのエラー解消
    ローカル環境で挙動確認したところ、エラーが見つかりました。
    スペルミスを改修しました。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
-      @order.seve
+      @order.save
       return redirect_to root_path
     else
       render 'index'
@@ -19,5 +19,5 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:price)
   end
-  
+
 end
