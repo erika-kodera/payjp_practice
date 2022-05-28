2a273eb0c261549cb204b3301e2f8da3b993a6fe
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-05-28 16:07:09 +0900

    クレジット決済の環境変数の実装。他
    ・クレジット決済に必要な記述、および公開するための環境変数を記述。
    ・ファクトリーボットにテスト用トークンを記述
    ・payjpのGemを追加。
    ・テストの実施

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,35 @@
 const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("フォーム送信時にイベント発火")
-});
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formDate = new FormData(formResult);
+
+    const card = {
+      number: formDate.get("order[number]"),
+      cvc: formDate.get("order[cvc]"),
+      exp_month: formDate.get("order[exp_month]"),
+      exp_year: `20${formDate.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200){
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("order_number").removeAttribute("name");
+      document.getElementById("order_cvc").removeAttribute("name");
+      document.getElementById("order_exp_month").removeAttribute("name");
+      document.getElementById("order_exp_year").removeAttribute("name");
+    
+      document.getElementById("charge-form").submit();
+    });
+  });
 };
 
 window.addEventListener("load", pay);
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,16 +6,22 @@ RSpec.describe Order, type: :model do
   end
 
   context '内容に問題ない場合' do
-    it "priceがあれば保存が出来ること" do
+    it "priceとtokenがあれば保存が出来ること" do
       expect(@order).to be_valid
     end
   end
 
   context '内容に問題がある場合' do
     it "priceが空では保存ができないこと" do
-    @order.price = nil
-    @order.valid?
-    expect(@order.errors.full_messages).to include("Price can't be blank")
+      @order.price = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "tokenが空では登録できないこと" do
+      @order.token = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Token can't be blank")
     end
   end
 end
